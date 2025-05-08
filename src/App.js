c00c3bd3869a567a1806c46c1637f580467e3eef
Jake Garcia <jake@realitycapturenetwork.com>
2025-05-07 22:16:09 -0600

    UserEmail component finished and the loading of the users email fromm the url parameters.

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function App() {
   const [formError, setFormError] = useState("");
   const [showError, setShowError] = useState(false);
   const [showSuccess, setShowSuccess] = useState(false);
-  const [email, setEmail] = useState("")
+  const [email, setEmail] = useState("");
 
   const checkboxHandler = (label) => {
     const checkboxCypher = {
@@ -45,16 +45,6 @@ function App() {
     changeFunc(!val);
   }
 
-  const getEmailParam = async () => {
-    const searchParams = new URLSearchParams(window.location.search);
-    const emailParam = searchParams.get('email');
-    if (emailParam) {
-      console.log(emailParam);
-      setEmail(emailParam)
-    }
-  }
-
-
   const handleFormSubmit = async (e) => {
     setFormError("");
     console.log("Form Submitted !");
@@ -64,23 +54,20 @@ function App() {
       recap: allChecked === true ? true : recapChecked,
       events: allChecked === true ? true : eventsChecked
     }
-    const emailParam = await getEmailParam();
-    if (emailParam !== false) {
-      const configuredPayload = {
-        email: emailParam,
-        lists: checkboxData
-      }
-      const updated = await contactUpdate(configuredPayload);
-      const {success} = updated;
-      if (success === true) {
-        setShowSuccess(true);
-      } else {
-        const {message} = updated
-        setFormError(message);
-        setShowError(true);  
-      }
-      setLoading(false);
+    const configuredPayload = {
+      email,
+      lists: checkboxData
     }
+    const updated = await contactUpdate(configuredPayload);
+    const {success} = updated;
+    if (success === true) {
+      setShowSuccess(true);
+    } else {
+      const {message} = updated
+      setFormError(message);
+      setShowError(true);  
+    }
+    setLoading(false);
   }
 
   const renderCheckboxes = () => {
@@ -106,13 +93,18 @@ function App() {
     return showError === true && <small className="text-red-500 font-sans-serif font-bold text-left">{formError}</small>
   }
 
-  const getUserEmail = () => {
-
+  const clearInput = () => {
+    console.log("Clear Input triggered!!")
+    setEmail("");
   }
 
-  useEffect(() => {
-    getUserEmail();
-  })
+  const handleEmailChange = (newText) => {
+    setEmail(newText);
+  }
+
+  // useEffect(() => {
+  //   getEmailParam();
+  // })
 
   return (
     <motion.div layout className="text-white w-full md:h-screen flex flex-col items-center justify-center relative bg-gradient-to-tl from-cloudline to-deep-wave z-0 transition-all duration-300">
@@ -125,7 +117,7 @@ function App() {
           <Success display={showSuccess} />
           <div className="w-full bg-white/10 p-4 flex flex-col gap-4 rounded-lg">
             {/* USER EMAIL */}
-            <UserEmail />
+            <UserEmail email={email} changeFunc={handleEmailChange} clearInput={clearInput} />
             <h4 className="">Check the box for the lists you'd like to join!</h4>
             <div className="flex flex-col gap-2">
               {renderCheckboxes()}
--- src/components/formComponents/UserEmail.js
+++ src/components/formComponents/UserEmail.js
@@ -1,12 +1,50 @@
-import { useState } from "react"
+import { useState, useEffect } from "react";
+import { LuDelete } from "react-icons/lu";
 
-const UserEmail = ({email, changeFunc}) => {
+const UserEmail = ({email, changeFunc, clearInput}) => {
 
     const [editing, setEditing] = useState(false);
+    const [pageLoaded, setPageLoading] = useState(false);
+
+    const renderEmail = () => {
+        if (editing) {
+            return (
+                <div className="w-1/2 rounded bg-white flex flex-row justify-between px-2">
+                    <input type="email" value={email} className="text-obsidian w-3/4" onChange={(e) => changeFunc(e.target.value)} />
+                    {email !== "" && 
+                        <LuDelete onClick={clearInput} size={24} color={"#212120"} className="hover:cursor-pointer hover:opacity-70 active:opacity-100 z-10" />
+                    }
+                </div>
+            )
+        } else {
+            return (
+                <div className="w-1/2 flex flex-row justify-start gap-8 items-end ">
+                    <p className="text-sm">{email}</p>
+                    <div onClick={() => setEditing(true)} className="text-deep-wave font-bold hover:cursor-pointer hover:text-blue-800 transition-all duration-300 text-xs">Not you? Change</div>
+                </div>
+            )
+        }
+    }
+
+    const getEmailParam = async () => {
+        const searchParams = new URLSearchParams(window.location.search);
+        const emailParam = searchParams.get('email');
+        if (emailParam) {
+          console.log(emailParam);
+          changeFunc(emailParam)
+        }
+      }
+
+    useEffect(() => {
+        if (pageLoaded === false) {
+            getEmailParam();
+        }
+    }, [pageLoaded])
 
     return (
         <div className="flex flex-col">
             <p className="">Email</p>
+            {renderEmail()}
         </div>
     )
 
