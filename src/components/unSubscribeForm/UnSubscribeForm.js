bd4a81a95b343693b493acabb62dc39269bb1ca7
Jake Garcia <jake@realitycapturenetwork.com>
2025-05-13 11:48:38 -0600

    Added in the unsubscribe button. Made its opacity changed based off of a regex expression to verify a valid email. Also gave this button an animated entrance.

--- src/components/unSubscribeForm/UnSubscribeForm.js
+++ src/components/unSubscribeForm/UnSubscribeForm.js
@@ -1,17 +1,34 @@
 import { motion } from "motion/react";
+import { useSelector } from "react-redux";
 
 import UserEmail from "../formComponents/UserEmail";
 
 
 const UnSubscribeForm = ({toggleModal}) => {
+
+    const email = useSelector(state => state.form.email);
+
+    const emailVerified = () => {
+        const emailRegex = /^[^@]+@[^@]+\.[^@]+$/;
+        return emailRegex.test(email);
+    }
+
     return (
-        <motion.div className="w-full bg-white/10 p-4 flex flex-col gap-4 rounded-lg relative"
+        <motion.div className="w-full bg-white/10 p-4 flex flex-col gap-4 rounded-lg relative overflow-hidden"
                 initial={{opacity: 0}}
                 animate={{opacity: 1}}
                 transition={{delay: 0.5, duration: 0.5}}
             >
-            <form action={toggleModal} className="w-full ">
+            <form action={toggleModal} className="w-full flex flex-col gap-4 ">
                 <UserEmail />
+                <motion.button
+                    className={`w-full rounded bg-white text-rcn-blue py-1 font-bold ${emailVerified() ? 'bg-opacity-100': "bg-opacity-20"} transiton-all duration-300`}
+                    initial={{opacity: 0, y: 50}}
+                    animate={{opacity: 1, y: 0}}
+                    transition={{delay: 1.25}}
+                >
+                    Unsubscribe
+                </motion.button>
             </form>
         </motion.div>
     )
