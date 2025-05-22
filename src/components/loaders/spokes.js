8345d76bb4c9a0b84a780fd87d182897451c5f89
Jake Garcia <jake@realitycapturenetwork.com>
2025-05-22 13:34:18 -0600

    Changed the loading animator to the rcn blue color.

--- src/components/loaders/spokes.js
+++ src/components/loaders/spokes.js
@@ -16,16 +16,16 @@ const Spokes = () => {
     })
 
     return (
-    <div className="relative h-9 w-9 flex flex-col items-center justify-between border border-neutral-white rounded-full transition-all duration-1000 animate-spin">
+    <div className="relative h-9 w-9 flex flex-col items-center justify-between border border-rcn-blue rounded-full transition-all duration-1000 animate-spin">
         <div className="w-full flex items-center justify-center">
-            <div className="bg-neutral-white h-3 w-1 rounded-t"></div>
+            <div className="bg-rcn-blue h-3 w-1 rounded-t"></div>
         </div>
         <div className={`w-full flex flex-row justify-between transiton-all duration-400 ${show ? 'scale-100 opacity-100' : 'opacity-0 scale-0'}`}>
-            <div className="bg-neutral-white w-3 h-1 rounded-l"></div>
-            <div className="bg-neutral-white w-3 h-1 rounded-r"></div>
+            <div className="bg-rcn-blue w-3 h-1 rounded-l"></div>
+            <div className="bg-rcn-blue w-3 h-1 rounded-r"></div>
         </div>
         <div className="w-full flex items-center justify-center">
-            <div className="bg-neutral-white h-3 w-1 rounded-b"></div>
+            <div className="bg-rcn-blue h-3 w-1 rounded-b"></div>
         </div>
     </div>
     )
